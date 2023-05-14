2e8c0be79de02db300216a028e5b14c0b1c36a7b
root <root@kali>
2023-05-14 07:18:10 -0400

    react app

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import {useEffect, useState} from 'react'
+import MovieCard from './MovieCard';
+import './App.css'
+import SearchIcon from './search.svg'
+
+const API_URL = 'http://www.omdbapi.com?apikey=be2e889';
+
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await  response.json();
+        setMovies(data.Search);
+    }
+    useEffect(() => {
+        searchMovies('world')
+    }, [])    
+
+    return (
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className='search'>
+                <input 
+                placeholder='Search for movies'
+                value={searchTerm}
+                onChange={(e)=>setSearchTerm(e.target.value)}
+                />
+                <img
+                src={SearchIcon}
+                alt="search"
+                onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+            {movies?.length>0
+                ? (
+                    <div className='container'> 
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie}/>
+                    ))}
+                    </div>
+                ): (
+                    <div className='empty'>
+                        <h2>No Movies found</h2>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+export default App 
---
+++ src/MovieCard.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+
+const MovieCard = ({ movie }) => {
+    return (
+        <div className='movie'>
+            <div>
+                <p>{movie.year}</p>
+            </div>
+            <div>
+                <img src={ movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
+            </div>
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    )
+}   
+export default MovieCard
